9eef89e3bdaff194acca8de10ad4425d6fb53250
johnnyreilly <johnny_reilly@hotmail.com>
2026-06-28 18:52:15 +0100

    chore: upgrade deps

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -127,7 +127,12 @@ const config: Config = {
   projectName: 'reillys-on-tour', // Usually your repo name.
 
   onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    }
+  },
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
