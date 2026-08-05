d06248139c83818843f0a039ef1408285b44fc8a
johnnyreilly <johnny_reilly@hotmail.com>
2026-08-05 12:06:14 +0100

    feat: RecoveryDiaries component

---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,7 @@
+import MDXComponents from '@theme-original/MDXComponents';
+import RecoveryDiaries from '@site/src/components/RecoveryDiaries.mdx';
+
+export default {
+  ...MDXComponents,
+  RecoveryDiaries,
+};
