ce28cd9c6c8d39c1b1cd8c7a9cc76b8e456e2879
John Reilly <johnny_reilly@hotmail.com>
2026-09-23 19:17:54 +0100

    Configure trailingSlash for GitHub Pages
    Set trailingSlash to false for GitHub Pages compatibility.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -117,6 +117,7 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/reillys-on-tour/',
+  trailingSlash: false, // 👈 Recommended for GitHub Pages
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
